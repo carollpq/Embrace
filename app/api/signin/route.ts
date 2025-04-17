fa31beaea9f124765c2217ed1e76d1faadd90064
carollpq <107604758+carollpq@users.noreply.github.com>
2025-04-17 23:15:04 +0800

    Fix Error in signin/route.ts

--- app/api/signin/route.ts
+++ app/api/signin/route.ts
@@ -51,6 +51,6 @@ export async function POST(req: Request) {
 
     return response;
   } catch (error) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
+    return NextResponse.json({ error: error }, { status: 500 });
   }
 }
