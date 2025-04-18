a38bf7b82604e169e1762493a2d75dc1b9a32434
carollpq <107604758+carollpq@users.noreply.github.com>
2025-04-18 10:24:22 +0800

    Add icon png

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ const raleway = Raleway({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Embrace",
   description: "Therapeutic Chatbot",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
