b1bb6c2725f42f0e8e2bc721d75eaf88ace8a391
carollpq <107604758+carollpq@users.noreply.github.com>
2025-04-25 11:02:29 +0800

    Fix Resizing for Toggle Image

--- components/ui/toggle.tsx
+++ components/ui/toggle.tsx
@@ -13,11 +13,17 @@ const Toggle: React.FC<ToggleProps> = () => {
     <>
       <Image
         alt="Toggle Icon"
-        src={nightMode ? "/icons/night-mode-toggle.png" : "/icons/day-mode-toggle.png"}
+        src={
+          nightMode
+            ? "/icons/night-mode-toggle.png"
+            : "/icons/day-mode-toggle.png"
+        }
         onClick={() => setNightMode(!nightMode)}
-        className="absolute left-8 bottom-5 cursor-pointer rounded-full p-1 transition-all duration-300"
-        width={105}
-        height={53}
+        className="absolute left-8 bottom-5 cursor-pointer rounded-full p-1 transition-all duration-300 
+             w-20 h-auto sm:w-24 md:w-28"
+        width={0} // Prevent Next.js warning
+        height={0}
+        sizes="(max-width: 640px) 80px, (max-width: 768px) 96px, 112px"
       />
     </>
   );
